f5394794f23e6a309e1a60ac9468aa61303ef24d
Colin Snover <github.com@zetafleet.com>
2017-11-12 17:05:04 -0600

    COMMON: Add support for arrays & limited custom deleters to smart pointers
    This enables ScopedPtr to be used for all cases where raw pointers
    are currently used to manage owned memory, and brings ScopedPtr
    closer to std::unique_ptr in terms of functionality. DisposablePtr
    also comes along for the ride.
    
    Custom deleters of ScopedPtr are not currently fully conforming to
    std::unique_ptr for the sake of simplicity of implementation.
    Unlike in the standard library, plain functions and lvalue
    references are not supported, nor may custom deleters be passed
    to the constructor at runtime. This can be improved in the future,
    if necessary, with zero overhead, by creating a Pair class that
    uses the Empty Base Optimization idiom to avoid extra storage
    overhead of the deleter instance when it is not needed, as in
    typical standard library implementations, plus some additional
    type traits to support the necessary metaprogramming for the
    different type overloads.
    
    Array types in SharedPtr also do not conform to C++17 which checks
    for convertability between the argument and an array type, but
    this should be an OK compromise for now.

--- common/ptr.h
+++ common/ptr.h
@@ -26,6 +26,7 @@
 #include "common/scummsys.h"
 #include "common/noncopyable.h"
 #include "common/safe-bool.h"
+#include "common/type-traits.h"
 #include "common/types.h"
 
 namespace Common {
@@ -40,13 +41,20 @@ class SharedPtrDeletionImpl : public SharedPtrDeletionInternal {
 public:
 	SharedPtrDeletionImpl(T *ptr) : _ptr(ptr) {}
 	~SharedPtrDeletionImpl() {
-		// Checks if the supplied type is not just a plain
-		// forward definition, taken from boost::checked_delete
-		// This makes the user really aware what he tries to do
-		// when using this with an incomplete type.
-		typedef char completeCheck[sizeof(T) ? 1 : -1];
-		(void)sizeof(completeCheck);
+		STATIC_ASSERT(sizeof(T) > 0, SharedPtr_cannot_delete_incomplete_type);
 		delete _ptr;
+	}
+private:
+	T *_ptr;
+};
+
+template<class T>
+class SharedPtrDeletionImpl<T []> : public SharedPtrDeletionInternal {
+public:
+	SharedPtrDeletionImpl(T ptr[]) : _ptr(ptr) {}
+	~SharedPtrDeletionImpl() {
+		STATIC_ASSERT(sizeof(T) > 0, SharedPtr_cannot_delete_incomplete_type);
+		delete[] _ptr;
 	}
 private:
 	T *_ptr;
@@ -110,14 +118,19 @@ class SharedPtr : public SafeBool<SharedPtr<T> > {
 #endif
 public:
 	typedef int RefValue;
-	typedef T ValueType;
-	typedef T *PointerType;
-	typedef T &ReferenceType;
+	typedef typename RemoveArray<T>::type ValueType;
+	typedef ValueType *PointerType;
+	typedef ValueType &ReferenceType;
 
-	SharedPtr() : _refCount(0), _deletion(0), _pointer(0) {}
+	SharedPtr() : _refCount(0), _deletion(nullptr), _pointer(nullptr) {}
 
-	template<class T2>
-	explicit SharedPtr(T2 *p) : _refCount(new RefValue(1)), _deletion(new SharedPtrDeletionImpl<T2>(p)), _pointer(p) {}
+	template<class T2, typename U = T, typename = typename EnableIf<IsArray<U>::value>::type>
+	explicit SharedPtr(T2 p) : _refCount(new RefValue(1)), _deletion(new SharedPtrDeletionImpl<typename RemovePointer<T2>::type []>(p)), _pointer(p) {
+	}
+
+	template<class T2, typename U = T, typename = typename DisableIf<IsArray<U>::value>::type>
+	explicit SharedPtr(T2 *p) : _refCount(new RefValue(1)), _deletion(new SharedPtrDeletionImpl<T2>(p)), _pointer(p) {
+	}
 
 	template<class T2, class D>
 	SharedPtr(T2 *p, D d) : _refCount(new RefValue(1)), _deletion(new SharedPtrDeletionDeleterImpl<T2, D>(p, d)), _pointer(p) {}
@@ -153,8 +166,22 @@ public:
 		return *this;
 	}
 
-	ReferenceType operator*() const { assert(_pointer); return *_pointer; }
-	PointerType operator->() const { assert(_pointer); return _pointer; }
+	template<typename U = T>
+	typename DisableIf<IsArray<U>::value, ReferenceType>::type operator*() const {
+		assert(_pointer);
+		return *_pointer;
+	}
+
+	template<typename U = T>
+	typename DisableIf<IsArray<U>::value, PointerType>::type operator->() const {
+		assert(_pointer);
+		return _pointer;
+	}
+
+	template<typename U = T>
+	typename EnableIf<IsArray<U>::value, ReferenceType>::type operator[](const int index) const {
+		return _pointer[index];
+	}
 
 	/**
 	 * Returns the plain pointer value. Be sure you know what you
@@ -178,13 +205,13 @@ public:
 	bool unique() const { return refCount() == 1; }
 
 	/**
-	 * Resets the SharedPtr object to a NULL pointer.
+	 * Resets the SharedPtr object to a null pointer.
 	 */
 	void reset() {
 		decRef();
-		_deletion = 0;
-		_refCount = 0;
-		_pointer = 0;
+		_refCount = nullptr;
+		_deletion = nullptr;
+		_pointer = nullptr;
 	}
 
 	template<class T2>
@@ -211,9 +238,9 @@ private:
 			if (!*_refCount) {
 				delete _refCount;
 				delete _deletion;
-				_deletion = 0;
-				_refCount = 0;
-				_pointer = 0;
+				_refCount = nullptr;
+				_deletion = nullptr;
+				_pointer = nullptr;
 			}
 		}
 	}
@@ -223,17 +250,44 @@ private:
 	PointerType _pointer;
 };
 
-template<typename T>
-class ScopedPtr : private NonCopyable, public SafeBool<ScopedPtr<T> > {
+template <typename T>
+struct DefaultDeleter {
+	inline void operator()(T *object) {
+		STATIC_ASSERT(sizeof(T) > 0, cannot_delete_incomplete_type);
+		delete object;
+	}
+};
+template <typename T>
+struct DefaultDeleter<T[]> {
+	inline void operator()(T object[]) {
+		STATIC_ASSERT(sizeof(T) > 0, cannot_delete_incomplete_type);
+		delete[] object;
+	}
+};
+
+template<typename T, class D = DefaultDeleter<T> >
+class ScopedPtr : private NonCopyable, public SafeBool<ScopedPtr<T, D> > {
 public:
-	typedef T ValueType;
-	typedef T *PointerType;
-	typedef T &ReferenceType;
+	typedef typename RemoveArray<T>::type ValueType;
+	typedef ValueType *PointerType;
+	typedef ValueType &ReferenceType;
 
-	explicit ScopedPtr(PointerType o = 0) : _pointer(o) {}
+	explicit ScopedPtr(PointerType o = nullptr) : _pointer(o) {}
 
-	ReferenceType operator*() const { return *_pointer; }
-	PointerType operator->() const { return _pointer; }
+	template<typename U = T>
+	typename DisableIf<IsArray<U>::value, ReferenceType>::type operator*() const {
+		return *_pointer;
+	}
+
+	template<typename U = T>
+	typename DisableIf<IsArray<U>::value, PointerType>::type operator->() const {
+		return _pointer;
+	}
+
+	template<typename U = T>
+	typename EnableIf<IsArray<U>::value, ReferenceType>::type operator[](const int index) const {
+		return _pointer[index];
+	}
 
 	/**
 	 * Implicit conversion operator to bool for convenience, to make
@@ -242,14 +296,14 @@ public:
 	bool operator_bool() const { return _pointer != nullptr; }
 
 	~ScopedPtr() {
-		delete _pointer;
+		D()(_pointer);
 	}
 
 	/**
 	 * Resets the pointer with the new value. Old object will be destroyed
 	 */
-	void reset(PointerType o = 0) {
-		delete _pointer;
+	void reset(PointerType o = nullptr) {
+		D()(_pointer);
 		_pointer = o;
 	}
 
@@ -268,7 +322,7 @@ public:
 	 */
 	PointerType release() {
 		PointerType r = _pointer;
-		_pointer = 0;
+		_pointer = nullptr;
 		return r;
 	}
 
@@ -276,22 +330,33 @@ private:
 	PointerType _pointer;
 };
 
-
-template<typename T>
-class DisposablePtr : private NonCopyable, public SafeBool<DisposablePtr<T> > {
+template<typename T, class D = DefaultDeleter<T> >
+class DisposablePtr : private NonCopyable, public SafeBool<DisposablePtr<T, D> > {
 public:
-	typedef T  ValueType;
-	typedef T *PointerType;
-	typedef T &ReferenceType;
+	typedef typename RemoveArray<T>::type ValueType;
+	typedef ValueType *PointerType;
+	typedef ValueType &ReferenceType;
 
 	explicit DisposablePtr(PointerType o, DisposeAfterUse::Flag dispose) : _pointer(o), _dispose(dispose) {}
 
 	~DisposablePtr() {
-		if (_dispose) delete _pointer;
+		if (_dispose) D()(_pointer);
 	}
 
-	ReferenceType operator*() const { return *_pointer; }
-	PointerType operator->() const { return _pointer; }
+	template<typename U = T>
+	typename DisableIf<IsArray<U>::value, ReferenceType>::type operator*() const {
+		return *_pointer;
+	}
+
+	template<typename U = T>
+	typename DisableIf<IsArray<U>::value, PointerType>::type operator->() const {
+		return _pointer;
+	}
+
+	template<typename U = T>
+	typename EnableIf<IsArray<U>::value, ReferenceType>::type operator[](const int index) const {
+		return _pointer[index];
+	}
 
 	/**
 	 * Implicit conversion operator to bool for convenience, to make
